7331e12ed74cd409ab064c98c9261543c2898a75
Manjul Chawla <33691894+manjulcr7@users.noreply.github.com>
2024-06-22 07:40:36 +0000

    challenge 6

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,12 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+Select * from customers where FirstName='Norby';
+
+--64
+select * from reservations where CustomerID=64;
+select * from reservations
+join customers on customers.CustomerID=reservations.CustomerID
+where customers.FirstName='Norby' and reservations.date > '2022-07-24';
+
+delete from reservations where ReservationId=2000;
