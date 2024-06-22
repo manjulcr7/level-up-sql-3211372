e5d5238fd18dfcc6deeb5dfd459b8e9f53e112b0
Manjul Chawla <33691894+manjulcr7@users.noreply.github.com>
2024-06-22 08:11:23 +0000

    challenge 8

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,16 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+select * from customers where LastName in
+ ('Stevensen', 'Stephensen', 'Stevenson', 'Stephenson', 'Stuyvesant');
+
+select * from Reservations where CustomerID in (select CustomerID from customers where LastName in
+ ('Stevensen', 'Stephensen', 'Stevenson', 'Stephenson', 'Stuyvesant'))
+ and partySize=4 and date >= '2022-06-14';
+
+select * from customers 
+join Reservations on Reservations.CustomerID=customers.CustomerID
+where customers.LastName in
+ ('Stevensen', 'Stephensen', 'Stevenson', 'Stephenson', 'Stuyvesant')
+and Reservations.partySize=4 and Reservations.date >= '2022-06-14'
