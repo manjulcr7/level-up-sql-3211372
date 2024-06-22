1a4bce69928ffa383d11631f3a22e24c465202ab
Manjul Chawla <33691894+manjulcr7@users.noreply.github.com>
2024-06-22 07:27:09 +0000

    challenge 5

--- 01_restaurant/challenges/01_05_updateinfo_ch.sql
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -2,3 +2,9 @@
 
 -- Taylor Jenkins, of 27170 6th Ave., Washington, DC,
 -- has moved to 74 Pine St., New York, NY.
+select CustomerID from Customers
+where FirstName='Taylor' and LastName='Jenkins' and Address='27170 6th Ave.';
+
+update Customers set address='74 Pine St.',city='New York',state='NY' 
+where FirstName='Taylor' and LastName='Jenkins'
+and Address='27170 6th Ave.';
