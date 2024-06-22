f9698debb3791639381e0e75d72107722cf1812d
Manjul Chawla <33691894+manjulcr7@users.noreply.github.com>
2024-06-22 12:21:56 +0000

    library challenge 8

--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -2,6 +2,15 @@
 
 -- Report 1: Show how many titles were published 
 -- in each year.
+select count(distinct(title)) as pubcount,published,
+group_concat(distinct(title)) from books 
+group by published
+order by pubcount desc;
 
+select * from books where published=1911;
 -- Report 2: Show the five books that have been
 -- checked out the most.
+select books.title, count(loans.loanid) as checkoutcount from books
+join loans on loans.bookid=books.bookid
+group by books.title
+order by checkoutcount desc;
