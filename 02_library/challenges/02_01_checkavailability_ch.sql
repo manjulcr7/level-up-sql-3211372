16955dc5eac16d4cbd761258868977307e31a2ba
Manjul Chawla <33691894+manjulcr7@users.noreply.github.com>
2024-06-22 09:27:21 +0000

    library challenge 1

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,13 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+select count() from books where title='Dracula';
+select count() from loans where 
+bookid in (select bookid from books where title='Dracula')
+and returneddate is null;
+
+select (
+(select count() from books where title='Dracula')-
+(select count() from loans where 
+bookid in (select bookid from books where title='Dracula')
+and returneddate is null)
+);
