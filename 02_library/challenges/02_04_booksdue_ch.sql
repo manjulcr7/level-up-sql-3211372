b1a62496f90feddcffbe65acf1963020165d9d5d
Manjul Chawla <33691894+manjulcr7@users.noreply.github.com>
2024-06-22 10:45:12 +0000

    library challenge 4

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,11 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+
+select books.title, loans.duedate,patrons.firstname,patrons.email
+from patrons 
+join loans on patrons.patronId=loans.patronId
+join books on books.BookID=loans.BookID
+where loans.duedate='2022-07-13' and loans.returneddate is null;
+
+select * from loans where duedate='2022-07-13' and returneddate is null;
