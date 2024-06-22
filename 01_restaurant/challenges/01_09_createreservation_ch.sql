341b82df7ec05341e6450d65a23c9cd35c2e499c
Manjul Chawla <33691894+manjulcr7@users.noreply.github.com>
2024-06-22 08:18:00 +0000

    challenge 9

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,11 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+select * from customers where email ='smac@kinetecoinc.com';
+insert into customers (FirstName,LastName,Email) values
+('Sam', 'McAdams' ,'smac@kinetecoinc.com');
+
+select * from Reservations where customerid=101;
+insert into Reservations (customerid,date,partysize)
+values (101,'2022-08-12 18:00:00',5);
