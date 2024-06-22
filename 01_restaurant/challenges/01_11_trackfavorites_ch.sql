4604470cc7b8ecb573aeee30f3450a11717c6f7f
Manjul Chawla <33691894+manjulcr7@users.noreply.github.com>
2024-06-22 08:39:40 +0000

    challenge 11

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,7 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+select * from customers where FirstName='Cleo';
+select dishid from dishes where name in ('Quinoa Salmon Salad');
+update customers set favoritedish=(select dishid from dishes where name in ('Quinoa Salmon Salad')) where FirstName='Cleo';
