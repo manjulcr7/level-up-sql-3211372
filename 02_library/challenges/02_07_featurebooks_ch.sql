ce6d252e1b63fc00ac90831b11860b08a98ee18e
Manjul Chawla <33691894+manjulcr7@users.noreply.github.com>
2024-06-22 12:10:49 +0000

    library challenge 7

--- 02_library/challenges/02_07_featurebooks_ch.sql
+++ 02_library/challenges/02_07_featurebooks_ch.sql
@@ -2,3 +2,17 @@
 
 -- Make a pick list of books published from 1890-1899 
 -- which are not currently checked out.
+select * from books where published>=1890 and published<=1899;
+select bookid from books where published>=1890 and published<=1899;
+
+--didn't work
+select books.title from books 
+join loans on books.bookid=loans.bookid
+where books.published>=1890 and books.published<=1899
+and loans.returneddate is not null order by books.title;
+
+--wrong as this will include
+select books.title from books 
+where books.published>=1890 and books.published<=1899
+and bookid not in (select bookid from loans where loans.returneddate is null)
+order by books.title;
