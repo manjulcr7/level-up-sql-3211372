e0105faa30af9290894dff7656c0d9bc195e346c
Manjul Chawla <33691894+manjulcr7@users.noreply.github.com>
2024-06-22 11:47:54 +0000

    library challenge 5

--- 02_library/challenges/02_05_returnbooks_ch.sql
+++ 02_library/challenges/02_05_returnbooks_ch.sql
@@ -4,3 +4,15 @@
 -- Book 1: 6435968624
 -- Book 2: 5677520613
 -- Book 3: 8730298424
+
+select * from loans where bookid in (46,73,105) and returneddate is null;
+select * from books;
+select * from books where barcode in (6435968624
+,5677520613,8730298424
+); 
+--46,73,105
+
+update loans set returneddate='2022-07-05'
+where bookid in (select bookid from books where barcode in (6435968624
+,5677520613,8730298424
+)) and returneddate is null;
