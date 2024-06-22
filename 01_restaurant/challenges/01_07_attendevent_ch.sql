6c19ed0f4cdb2970538f76214dd89e3ddcc0e871
Manjul Chawla <33691894+manjulcr7@users.noreply.github.com>
2024-06-22 08:01:02 +0000

    challenge 7

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,6 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+SELECT customerid from customers where Email='atapley2j@kinetecoinc.com';
+select * from customerResponses;
+insert into customerResponses (customerid,partySize) values ((SELECT customerid from customers where Email='atapley2j@kinetecoinc.com'),3);
