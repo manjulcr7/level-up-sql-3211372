53e2c589914a35340168a76cce40f169e4c9c10b
Manjul Chawla <33691894+manjulcr7@users.noreply.github.com>
2024-06-22 11:54:53 +0000

    library challenge 6

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,11 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+
+select patrons.firstname,patrons.email, count(loanid) as booksissued from patrons 
+join loans on patrons.patronid=loans.patronid
+group by patrons.patronid order by booksissued asc;
+
+select 
+select * from patrons 
+join loans on patrons.patronid=loans.patronid
+group by patrons.patronid;
